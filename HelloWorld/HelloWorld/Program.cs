e1535609569fe7400e52d00a68ee656fa26c736e
a2davidr <roedavidc@yahoo.com>
2014-06-26 11:02:59 -0400

    Hello World
    Created Hello World app.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputReadLine = " ";
+            Console.WriteLine(" ");
+            Console.WriteLine("Hello World");
+            Console.WriteLine(" ");
+            inputReadLine = Console.ReadLine();
+            Console.WriteLine(" ");
+            Console.WriteLine(inputReadLine);
+            Console.ReadKey();
+        }
+    }
+}
